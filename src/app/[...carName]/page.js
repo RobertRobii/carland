31795e755d800e6437cdbeff9abbb355683a22d3
RobertRobii <robertraiovici01@gmail.com>
2024-11-24 15:18:03 +0200

    commented some code for debuggin

--- src/app/[...carName]/page.js
+++ src/app/[...carName]/page.js
@@ -146,75 +146,75 @@ const CarDetails = ({ params }) => {
 
       console.log("data:", data);
 
-      if (data.success) {
-        // const emailTemplate = `
-        //     <html>
-        //       <body>
-        //         <h3>Here are the details of your rental:</h3>
-        //         <div style="width: 400px; display: flex; justify-content: space-between; margin-bottom: 20px; text-align: center;">
-        //           <div style="width: 100px; margin-right: auto; background-color: #FF6969; border: 2px solid #FF0000; font-weight: bold; padding: 10px; border-radius: 10px;">Car: ${
-        //             selectedCar.name
-        //           }</div>
-        //           <div style="width: 100px; background-color: #FF6969; border: 2px solid #FF0000; font-weight: bold; padding: 10px; border-radius: 10px;">Location: ${selectedLocation}</div>
-        //         </div>
-        //         <div style="width: 400px; display: flex; justify-content: space-between; margin-bottom: 20px; text-align: center;">
-        //           <div style="width: 100px; margin-right: auto; background-color: #FF6969; border: 2px solid #FF0000; font-weight: bold; padding: 10px; border-radius: 10px;">Pick up time: ${format(
-        //             selectedDate[0].startDate,
-        //             "dd.MM.yyyy"
-        //           )} at ${selectedHours}</div>
-        //         <div style="width: 100px; background-color: #FF6969; border: 2px solid #FF0000; font-weight: bold; padding: 10px; border-radius: 10px;">Return time: ${format(
-        //           selectedDate[0].endDate,
-        //           "dd.MM.yyyy"
-        //         )} at ${selectedHours}</div>
-        //         </div>
-        //         <div style="width: 400px; display: flex; justify-content: space-between; margin-bottom: 20px; text-align: center;">
-        //           <div style="width: 100px; margin-right: auto; background-color: #FF6969; border: 2px solid #FF0000; font-weight: bold; padding: 10px; border-radius: 10px;">Price per day: ${
-        //             selectedCar.price
-        //           }€</div>
-        //         <div style="width: 100px; background-color: #FF6969; border: 2px solid #FF0000; font-weight: bold; padding: 10px; border-radius: 10px;">Total price: ${
-        //           adjustedDaysDifference * selectedCar.price
-        //         }€</div>
-        //         </div>
-        //         <h5>Thank you for choosing us!</h5>
-        //       </body>
-        //     </html>
-        //     `;
+      // if (data.success) {
+      //   const emailTemplate = `
+      //       <html>
+      //         <body>
+      //           <h3>Here are the details of your rental:</h3>
+      //           <div style="width: 400px; display: flex; justify-content: space-between; margin-bottom: 20px; text-align: center;">
+      //             <div style="width: 100px; margin-right: auto; background-color: #FF6969; border: 2px solid #FF0000; font-weight: bold; padding: 10px; border-radius: 10px;">Car: ${
+      //               selectedCar.name
+      //             }</div>
+      //             <div style="width: 100px; background-color: #FF6969; border: 2px solid #FF0000; font-weight: bold; padding: 10px; border-radius: 10px;">Location: ${selectedLocation}</div>
+      //           </div>
+      //           <div style="width: 400px; display: flex; justify-content: space-between; margin-bottom: 20px; text-align: center;">
+      //             <div style="width: 100px; margin-right: auto; background-color: #FF6969; border: 2px solid #FF0000; font-weight: bold; padding: 10px; border-radius: 10px;">Pick up time: ${format(
+      //               selectedDate[0].startDate,
+      //               "dd.MM.yyyy"
+      //             )} at ${selectedHours}</div>
+      //           <div style="width: 100px; background-color: #FF6969; border: 2px solid #FF0000; font-weight: bold; padding: 10px; border-radius: 10px;">Return time: ${format(
+      //             selectedDate[0].endDate,
+      //             "dd.MM.yyyy"
+      //           )} at ${selectedHours}</div>
+      //           </div>
+      //           <div style="width: 400px; display: flex; justify-content: space-between; margin-bottom: 20px; text-align: center;">
+      //             <div style="width: 100px; margin-right: auto; background-color: #FF6969; border: 2px solid #FF0000; font-weight: bold; padding: 10px; border-radius: 10px;">Price per day: ${
+      //               selectedCar.price
+      //             }€</div>
+      //           <div style="width: 100px; background-color: #FF6969; border: 2px solid #FF0000; font-weight: bold; padding: 10px; border-radius: 10px;">Total price: ${
+      //             adjustedDaysDifference * selectedCar.price
+      //           }€</div>
+      //           </div>
+      //           <h5>Thank you for choosing us!</h5>
+      //         </body>
+      //       </html>
+      //       `;
 
-        // await fetch("/api/sendEmail", {
-        //   method: "POST",
-        //   headers: {
-        //     "Content-Type": "application/json",
-        //   },
-        //   body: JSON.stringify({
-        //     name: fullname,
-        //     email: email,
-        //     message: emailTemplate,
-        //   }),
-        // });
+      //   await fetch("/api/sendEmail", {
+      //     method: "POST",
+      //     headers: {
+      //       "Content-Type": "application/json",
+      //     },
+      //     body: JSON.stringify({
+      //       name: fullname,
+      //       email: email,
+      //       message: emailTemplate,
+      //     }),
+      //   });
 
-        // console.log("Email sent successfully");
-        // toast.success(
-        //   "Rental confirmed. A confirmation mail has been sent to you!",
-        //   { duration: 5000 }
-        // );
-        setFullname("");
-        setEmail("");
-        setPhone("");
-        setSelectedLocation(null);
-        setSelectedDate(null);
-        setSelectedHours(null);
-        setErrorMessage("");
-        // router.push("/");
-      } else {
-        console.error(
-          "Error while sending data!",
-          "data message:",
-          data.message
-        );
-        toast.error(data.message || "Failed to rent car. Please try again.", {
-          duration: 5000,
-        });
-      }
+      //   console.log("Email sent successfully");
+      //   toast.success(
+      //     "Rental confirmed. A confirmation mail has been sent to you!",
+      //     { duration: 5000 }
+      //   );
+      //   setFullname("");
+      //   setEmail("");
+      //   setPhone("");
+      //   setSelectedLocation(null);
+      //   setSelectedDate(null);
+      //   setSelectedHours(null);
+      //   setErrorMessage("");
+      //   // router.push("/");
+      // } else {
+      //   console.error(
+      //     "Error while sending data!",
+      //     "data message:",
+      //     data.message
+      //   );
+      //   toast.error(data.message || "Failed to rent car. Please try again.", {
+      //     duration: 5000,
+      //   });
+      // }
     } catch (error) {
       console.log("Error while sending data:", error);
       toast.error("Failed to rent car. Please try again.", { duration: 5000 });
