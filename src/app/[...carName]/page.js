98d7d1351d366f618e11c9c88c55dc2515f055d5
RobertRobii <robertraiovici01@gmail.com>
2024-11-30 11:46:19 +0200

    commented the send email code

--- src/app/[...carName]/page.js
+++ src/app/[...carName]/page.js
@@ -147,50 +147,50 @@ const CarDetails = ({ params }) => {
         setSelectedHours(null);
         setErrorMessage("");
 
-        const emailTemplate = `
-            <html>
-              <body>
-                <h3>Here are the details of your rental:</h3>
-                <div style="width: 400px; display: flex; justify-content: space-between; margin-bottom: 20px; text-align: center;">
-                  <div style="width: 100px; margin-right: auto; background-color: #FF6969; border: 2px solid #FF0000; font-weight: bold; padding: 10px; border-radius: 10px;">Car: ${
-                    selectedCar.name
-                  }</div>
-                  <div style="width: 100px; background-color: #FF6969; border: 2px solid #FF0000; font-weight: bold; padding: 10px; border-radius: 10px;">Location: ${selectedLocation}</div>
-                </div>
-                <div style="width: 400px; display: flex; justify-content: space-between; margin-bottom: 20px; text-align: center;">
-                  <div style="width: 100px; margin-right: auto; background-color: #FF6969; border: 2px solid #FF0000; font-weight: bold; padding: 10px; border-radius: 10px;">Pick up time: ${format(
-                    selectedDate[0].startDate,
-                    "dd.MM.yyyy"
-                  )} at ${selectedHours}</div>
-                <div style="width: 100px; background-color: #FF6969; border: 2px solid #FF0000; font-weight: bold; padding: 10px; border-radius: 10px;">Return time: ${format(
-                  selectedDate[0].endDate,
-                  "dd.MM.yyyy"
-                )} at ${selectedHours}</div>
-                </div>
-                <div style="width: 400px; display: flex; justify-content: space-between; margin-bottom: 20px; text-align: center;">
-                  <div style="width: 100px; margin-right: auto; background-color: #FF6969; border: 2px solid #FF0000; font-weight: bold; padding: 10px; border-radius: 10px;">Price per day: ${
-                    selectedCar.price
-                  }€</div>
-                <div style="width: 100px; background-color: #FF6969; border: 2px solid #FF0000; font-weight: bold; padding: 10px; border-radius: 10px;">Total price: ${
-                  adjustedDaysDifference * selectedCar.price
-                }€</div>
-                </div>
-                <h5>Thank you for choosing us!</h5>
-              </body>
-            </html>
-            `;
+        // const emailTemplate = `
+        //     <html>
+        //       <body>
+        //         <h3>Here are the details of your rental:</h3>
+        //         <div style="width: 400px; display: flex; justify-content: space-between; margin-bottom: 20px; text-align: center;">
+        //           <div style="width: 100px; margin-right: auto; background-color: #FF6969; border: 2px solid #FF0000; font-weight: bold; padding: 10px; border-radius: 10px;">Car: ${
+        //             selectedCar.name
+        //           }</div>
+        //           <div style="width: 100px; background-color: #FF6969; border: 2px solid #FF0000; font-weight: bold; padding: 10px; border-radius: 10px;">Location: ${selectedLocation}</div>
+        //         </div>
+        //         <div style="width: 400px; display: flex; justify-content: space-between; margin-bottom: 20px; text-align: center;">
+        //           <div style="width: 100px; margin-right: auto; background-color: #FF6969; border: 2px solid #FF0000; font-weight: bold; padding: 10px; border-radius: 10px;">Pick up time: ${format(
+        //             selectedDate[0].startDate,
+        //             "dd.MM.yyyy"
+        //           )} at ${selectedHours}</div>
+        //         <div style="width: 100px; background-color: #FF6969; border: 2px solid #FF0000; font-weight: bold; padding: 10px; border-radius: 10px;">Return time: ${format(
+        //           selectedDate[0].endDate,
+        //           "dd.MM.yyyy"
+        //         )} at ${selectedHours}</div>
+        //         </div>
+        //         <div style="width: 400px; display: flex; justify-content: space-between; margin-bottom: 20px; text-align: center;">
+        //           <div style="width: 100px; margin-right: auto; background-color: #FF6969; border: 2px solid #FF0000; font-weight: bold; padding: 10px; border-radius: 10px;">Price per day: ${
+        //             selectedCar.price
+        //           }€</div>
+        //         <div style="width: 100px; background-color: #FF6969; border: 2px solid #FF0000; font-weight: bold; padding: 10px; border-radius: 10px;">Total price: ${
+        //           adjustedDaysDifference * selectedCar.price
+        //         }€</div>
+        //         </div>
+        //         <h5>Thank you for choosing us!</h5>
+        //       </body>
+        //     </html>
+        //     `;
 
-        await fetch("/api/sendEmail", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            name: fullname,
-            email: email,
-            message: emailTemplate,
-          }),
-        });
+        // await fetch("/api/sendEmail", {
+        //   method: "POST",
+        //   headers: {
+        //     "Content-Type": "application/json",
+        //   },
+        //   body: JSON.stringify({
+        //     name: fullname,
+        //     email: email,
+        //     message: emailTemplate,
+        //   }),
+        // });
 
         console.log("Email sent successfully");
         toast.success(
